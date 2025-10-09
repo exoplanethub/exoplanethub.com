84a1f48c7ae8f6bb35b1a7abf7a6fdd4353f8782
Zack <zackrylangford@gmail.com>
2025-10-09 09:10:38 -0400

    move jwts image to local

--- exoplanethub.com/components/home/Hero.tsx
+++ exoplanethub.com/components/home/Hero.tsx
@@ -1,28 +1,12 @@
-'use client';
-import { useEffect, useState } from 'react';
 import styles from './Hero.module.css';
 
-const JWST_IMAGES = [
-  'https://science.nasa.gov/wp-content/uploads/2023/09/web-first-images-release.png',
-  'https://science.nasa.gov/wp-content/uploads/2023/04/potw2316a.jpg',
-  'https://science.nasa.gov/wp-content/uploads/2023/09/stsci-01gfnn3pwjmy4rqxkz585bc4qh.png'
-];
-
 export default function Hero() {
-  const [bgImage, setBgImage] = useState('');
-
-  useEffect(() => {
-    setBgImage(JWST_IMAGES[Math.floor(Math.random() * JWST_IMAGES.length)]);
-  }, []);
-
   return (
     <section className={styles.hero}>
-      {bgImage && (
-        <div 
-          className={styles.background}
-          style={{ backgroundImage: `url(${bgImage})` }}
-        />
-      )}
+      <div 
+        className={styles.background}
+        style={{ backgroundImage: 'url(/web-first-images-release.png)' }}
+      />
       <div className={styles.overlay}></div>
       <div className={styles.stars}></div>
       <div className={styles.container}>
